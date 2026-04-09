0164c398b4608e3f73301cf4ffb30e6d9ee90ff3
Boostdoo <boostdoo.dev@gmail.com>
2026-04-09 20:47:36 +0200

    Update banner image to banner.gif in __manifest__.py

--- boostdoo_command_palette/__manifest__.py
+++ boostdoo_command_palette/__manifest__.py
@@ -41,7 +41,7 @@ Features:
             'boostdoo_command_palette/static/src/css/command_palette.scss',
         ],
     },
-    'image': 'static/description/banner.png',
+    'images': ['static/description/banner.gif'],
     'installable': True,
     'application': False,
     'auto_install': False,
